9649b9eeab70020f3a6ad8598eab2f158f369125
Banson <banson94113@hotmail.com>
2015-10-22 23:20:46 +1100

    shader code test

--- src/ass2/spec/Game.java
+++ src/ass2/spec/Game.java
@@ -5,11 +5,15 @@ import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLJPanel;
 import javax.media.opengl.glu.GLU;
 import javax.swing.JFrame;
+
+import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.util.texture.Texture;
@@ -34,6 +38,41 @@ public class Game extends JFrame implements GLEventListener, KeyListener{
     private Texture leaves;
 
     private boolean thirdperson = false;
+
+    //shader code
+    private float positions[] =  {0,1,-1,
+            -1,-1,-1,
+            1,-1,-1,
+            0, 2,-4,
+            -2,-2,-4,
+            2,-2,-4};
+
+    //There should be a matching entry in this array for each entry in
+    //the positions array
+    private float colors[] =     {1,0,0,
+            0,1,0,
+            1,1,1,
+            0,0,0,
+            0,0,1,
+            1,1,0};
+
+    //Best to use smallest data type possible for indexes
+    //We could even use byte here...
+    private short indexes[] = {0,1,5,3,4,2};
+
+    //These are not vertex buffer objects, they are just java containers
+    private FloatBuffer posData= Buffers.newDirectFloatBuffer(positions);
+    private FloatBuffer colorData = Buffers.newDirectFloatBuffer(colors);
+    private ShortBuffer indexData = Buffers.newDirectShortBuffer(indexes);
+
+    //We will be using 2 vertex buffer objects
+    private int bufferIds[] = new int[2];
+
+
+    private static final String VERTEX_SHADER = "AttributeVertex.glsl";
+    private static final String FRAGMENT_SHADER = "AttributeFragment.glsl";
+
+    private int shaderprogram;
 
 
     public Game(Terrain terrain) {
@@ -137,6 +176,34 @@ public class Game extends JFrame implements GLEventListener, KeyListener{
 
         // draw the scene tree
         GameObject.ROOT.draw(gl);*/
+
+        //shader code
+        //Use the shader
+        gl.glUseProgram(shaderprogram);
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER,bufferIds[0]);
+
+        int vertexColLoc = gl.glGetAttribLocation(shaderprogram,"vertexCol");
+        int vertexPosLoc = gl.glGetAttribLocation(shaderprogram,"vertexPos");
+
+        // Specify locations for the co-ordinates and color arrays.
+        gl.glEnableVertexAttribArray(vertexPosLoc);
+        gl.glEnableVertexAttribArray(vertexColLoc);
+        gl.glVertexAttribPointer(vertexPosLoc,3, GL.GL_FLOAT, false,0, 0); //last num is the offset
+        gl.glVertexAttribPointer(vertexColLoc,3, GL.GL_FLOAT, false,0, positions.length*Float.BYTES);
+
+
+        gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, bufferIds[1]);
+
+
+        gl.glDrawElements(GL2.GL_TRIANGLES, 6, GL2.GL_UNSIGNED_SHORT,0);
+
+
+        gl.glUseProgram(0);
+
+        //Un-bind the buffer.
+        //This is not needed in this simple example but good practice
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER,0);
+        gl.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER,0);
     }
 
 	@Override
@@ -169,6 +236,51 @@ public class Game extends JFrame implements GLEventListener, KeyListener{
         gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
         gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR_MIPMAP_LINEAR);
         gl.glGenerateMipmap(GL2.GL_TEXTURE_2D);
+
+        //shader code
+        //Generate 2 VBO buffer and get their IDs
+        gl.glGenBuffers(2,bufferIds,0);
+
+        //This buffer is now the current array buffer
+        //array buffers hold vertex attribute data
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER,bufferIds[0]);
+
+        //This is just setting aside enough empty space
+        //for all our data
+        gl.glBufferData(GL2.GL_ARRAY_BUFFER,    //Type of buffer
+                positions.length * Float.BYTES +  colors.length* Float.BYTES, //size needed
+                null,    //We are not actually loading data here yet
+                GL2.GL_STATIC_DRAW); //We expect once we load this data we will not modify it
+
+
+        //Actually load the positions data
+        gl.glBufferSubData(GL2.GL_ARRAY_BUFFER, 0, //From byte offset 0
+                positions.length*Float.BYTES,posData);
+
+        //Actually load the color data
+        gl.glBufferSubData(GL2.GL_ARRAY_BUFFER,
+                positions.length*Float.BYTES,  //Load after the position data
+                colors.length*Float.BYTES,colorData);
+
+
+        //Now for the element array
+        //Element arrays hold indexes to an array buffer
+        gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, bufferIds[1]);
+
+        //We can load it all at once this time since there are not
+        //two separate parts like there was with color and position.
+        gl.glBufferData(GL2.GL_ELEMENT_ARRAY_BUFFER,
+                indexes.length *Short.BYTES,
+                indexData, GL2.GL_STATIC_DRAW);
+
+        try {
+            shaderprogram = Shader.initShaders(gl,VERTEX_SHADER,FRAGMENT_SHADER);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
 	@Override
